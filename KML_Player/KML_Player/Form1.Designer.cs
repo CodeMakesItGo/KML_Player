d372595c7d3e4a238f1254247b58e542a83ed5dc
CodeMakesItGo <jason.altice@codemakesitgo.com>
2020-12-16 17:18:04 -0700

    Add files via upload

---
+++ KML_Player/KML_Player/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace KML_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_open = new System.Windows.Forms.Button();
+            this.textBox_file = new System.Windows.Forms.TextBox();
+            this.button_play = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(12, 12);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // textBox_file
+            // 
+            this.textBox_file.Location = new System.Drawing.Point(105, 12);
+            this.textBox_file.Name = "textBox_file";
+            this.textBox_file.Size = new System.Drawing.Size(279, 20);
+            this.textBox_file.TabIndex = 1;
+            // 
+            // button_play
+            // 
+            this.button_play.Location = new System.Drawing.Point(12, 66);
+            this.button_play.Name = "button_play";
+            this.button_play.Size = new System.Drawing.Size(75, 23);
+            this.button_play.TabIndex = 2;
+            this.button_play.Text = "Play";
+            this.button_play.UseVisualStyleBackColor = true;
+            this.button_play.Click += new System.EventHandler(this.button_play_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(105, 66);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 2;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(202, 71);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(31, 13);
+            this.label_status.TabIndex = 3;
+            this.label_status.Text = "none";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 113);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_play);
+            this.Controls.Add(this.textBox_file);
+            this.Controls.Add(this.button_open);
+            this.Name = "Form1";
+            this.Text = "KML Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.TextBox textBox_file;
+        private System.Windows.Forms.Button button_play;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ KML_Player/KML_Player/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KmlWriter;
+
+namespace KML_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Stop();
+        }
+
+        private ReadFlightLog ReadFlightLog = new ReadFlightLog();
+        private KmlWriter.KmlWriter KmlWriter = new KmlWriter.KmlWriter();
+        private List<Tuple<double, double, double>> path = new List<Tuple<double, double, double>>();
+        private int counter = 0;
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox_file.Text = ofd.FileName;
+            }
+        }
+
+        private void button_play_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(textBox_file.Text))
+            {
+                if(!ReadFlightLog.ReadFile(textBox_file.Text))
+                {
+                    MessageBox.Show("Error finding flight data");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File does not exist");
+                return;
+            }
+
+            path.Clear();
+            counter = 0;
+            timer1.Start();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var data = ReadFlightLog.GetPosition(counter);
+
+            if(data != null)
+            {
+                path.Add(new Tuple<double, double, double>(data.lat, data.lon, data.alt));
+
+                KmlWriter.UpdateKmlPath(path, "Hdg:" + data.hdg);
+                KmlWriter.UpdateKmlVehicle(data.lat, data.lon, data.alt, data.hdg, counter.ToString(), "Test");
+
+                label_status.Text = $"{counter}: Lat:{data.lat} Lon:{data.lon}";
+                counter += 12;//log rate is about 25 messages per second, so skip some a 2Hz
+            }
+        }
+    }
+}
